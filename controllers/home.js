d87f478b06959f2a1c098f795fb60cf29a863d4c
gouthamkreddy <greddy@cse.iitk.ac.in>
2016-10-01 13:21:59 +0530

    twilio integrated

--- controllers/home.js
+++ controllers/home.js
@@ -8,8 +8,6 @@ exports.index = function(req, res) {
 exports.msg = function(req, res) {
 	console.log(req.body);
 
-
-
 	// read url, phone number, type
 		
 	/*--- wikipedia ---*/
@@ -20,7 +18,6 @@ exports.msg = function(req, res) {
 	      console.log("An error occurred[query=%s, error=%s]", query, err);
 	      return;
 	    }
-
 	    res.send(htmlWikiText);  
 	});
 
@@ -31,4 +28,20 @@ exports.msg = function(req, res) {
 	// get data and parse
 
 	// send response
+	var accountSid = 'ACb412781167345e15410b639433dd90ec';
+	var authToken = '2471d21ed02bfe0a50390a36058f2415';
+	//require the Twilio module and create a REST client
+	var client = require('twilio')(accountSid, authToken);
+	client.messages.create({
+	  to: "+917752846564",
+	  from: "+12058720018",
+	  body: "Tomorrow's forecast in Financial District, San Francisco is Clear."
+	}, function(err, message) {
+	  if (err) {
+	    console.error(err);
+	  } else {
+	    console.log(message);
+	  }
+
+	});
 };
